703938d074282cc9b037211dabdfee3cb6745a23
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-02-24 12:47:55 +0000

    main

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,17 @@
 <script>
-	import '../app.css';
+  import '../app.css';
 </script>
+
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl normal-case">dogInder</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal p-0">
+      <li><a href="/liked"> ❤️ </a></li>
+      <!-- <li><a>About</a></li> -->
+    </ul>
+  </div>
+</div>
 
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,50 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<div class="mockup-code">
-  <pre data-prefix="$"><code>npm i daisyui</code></pre>
-  <pre data-prefix=">" class="text-warning"><code>installing...</code></pre>
-  <pre data-prefix=">" class="text-success"><code>Done!</code></pre>
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let imageUrl = '';
+
+  const loadDogImage = async () => {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch(url);
+    imageUrl = response.ok && (await response.json()).message;
+  };
+
+  const saveLikedDog = () => {
+    const listName = 'likedDogs';
+    const dogsList = JSON.parse(localStorage.getItem(listName)) || [];
+    dogsList.push(imageUrl);
+    localStorage.setItem(listName, JSON.stringify(dogsList));
+  };
+
+  onMount(async () => {
+    await loadDogImage();
+  });
+</script>
+
+<div class="grid h-full place-items-center">
+  <div class="card w-96 bg-base-200 shadow-xl">
+    <figure><img class="h-60 object-cover" src={imageUrl} alt="Dog" /></figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Do You ❤️ This 🐶 ?</h2>
+      <div class="card-actions justify-center ">
+        <button
+          class="btn-outline btn btn-secondary btn-lg"
+          on:click={async () => {
+            await loadDogImage();
+            saveLikedDog();
+          }}
+        >
+          ❤️
+        </button>
+        <button
+          class="btn-outline btn btn-primary btn-lg"
+          on:click={async () => {
+            await loadDogImage();
+          }}
+        >
+          💔
+        </button>
+      </div>
+    </div>
+  </div>
 </div>
