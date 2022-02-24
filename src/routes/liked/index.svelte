6930b26eb4957b8d69ec15d4d764f3f566d9df09
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-02-24 16:33:59 +0000

    add delete button

--- src/routes/liked/index.svelte
+++ src/routes/liked/index.svelte
@@ -1,16 +1,46 @@
 <script lang="ts">
   import Lazy from 'svelte-lazy';
   import { likedDogsList } from '$lib/store';
+
+  let show: string = null;
+
+  function toggleShow(url: string) {
+    show = show ? null : url;
+  }
+
+  const deleteDog = (url: string) => {
+    $likedDogsList = $likedDogsList.filter((dogUrl) => dogUrl !== url);
+    if (show === url) {
+      show = null;
+    }
+  };
 </script>
 
 <div class="grid h-full grid-cols-1 place-items-center gap-4 md:grid-cols-2">
   {#each $likedDogsList as src}
     <Lazy height={300} offset={0}>
-      <div class="card w-96 bg-base-200 shadow-xl">
+      <div
+        class="card relative w-96 bg-base-200 shadow-xl"
+        on:mouseenter={() => toggleShow(src)}
+        on:mouseleave={() => toggleShow(src)}
+      >
         <figure>
           <img class="h-60 object-cover" {src} alt="Dog" />
         </figure>
+        {#if show === src}
+          <button class="btn btn-error btn-circle absolute top-0 right-0" on:click={() => deleteDog(src)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg
+            >
+          </button>
+        {/if}
       </div>
     </Lazy>
   {/each}
 </div>
+
