18fcd0b91fe361aca440d5be4fe844a6c26f967d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-02-24 14:44:15 +0000

    move url to module etc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,57 @@
-<script lang="ts">
-  import { onMount } from 'svelte';
+<script context="module">
+  const url = 'https://dog.ceo/api/breeds/image/random';
+  export async function load({ fetch }) {
+    const response = await fetch(url);
 
-  let imageUrl = '';
+    return {
+      status: response.status,
+      props: {
+        dogRes: response.ok && (await response.json()),
+        url: url,
+      },
+    };
+  }
+</script>
+
+<!-- </script> -->
+<script lang="ts">
+  export let dogRes: { message: string };
+
+  let imageUrl = dogRes.message;
+
+  import { onMount } from 'svelte';
+  import { get } from 'svelte/store';
+  import { likedDogsList } from '$lib/store';
 
   const loadDogImage = async () => {
-    const url = 'https://dog.ceo/api/breeds/image/random';
     const response = await fetch(url);
-    imageUrl = response.ok && (await response.json()).message;
+    if (response.ok) {
+      const dogRes = await response.json();
+      imageUrl = dogRes.message;
+    }
   };
 
-  const saveLikedDog = () => {
-    const listName = 'likedDogs';
-    const dogsList = JSON.parse(localStorage.getItem(listName)) || [];
-    dogsList.push(imageUrl);
-    localStorage.setItem(listName, JSON.stringify(dogsList));
+  const saveLikedDogsURL = () => {
+    likedDogsList.set([...get(likedDogsList), imageUrl]);
   };
-
   onMount(async () => {
-    await loadDogImage();
+    imageUrl = dogRes.message;
   });
 </script>
 
 <div class="grid h-full place-items-center">
   <div class="card w-96 bg-base-200 shadow-xl">
-    <figure><img class="h-60 object-cover" src={imageUrl} alt="Dog" /></figure>
+    <figure>
+      <img class="h-60 object-cover" src={imageUrl} alt="Dog" />
+    </figure>
     <div class="card-body items-center text-center">
       <h2 class="card-title">Do You ❤️ This 🐶 ?</h2>
       <div class="card-actions justify-center ">
         <button
           class="btn-outline btn btn-secondary btn-lg"
           on:click={async () => {
+            saveLikedDogsURL();
             await loadDogImage();
-            saveLikedDog();
           }}
         >
           ❤️
