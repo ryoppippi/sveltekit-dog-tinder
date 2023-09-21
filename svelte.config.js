9ae6c8d2442ae0e4d3bedc147aa95573f1d01f36
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-21 15:42:35 +0100

    format

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
