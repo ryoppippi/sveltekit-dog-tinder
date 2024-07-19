1e2f6d155238f44ede02c3eea8ee7be8d767dabe
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-19 20:43:51 +0100

    format

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,12 +1,10 @@
-<script lang="ts">
+<script lang='ts'>
 	import { fade, fly, scale } from 'svelte/transition';
 	import { LikedDogsList } from '$lib/rune.svelte.js';
 
 	const likedDogsList = new LikedDogsList();
 
-	const { onbuttonTapped } = $props<{
-		onbuttonTapped: () => void;
-	}>();
+	const { onbuttonTapped }: { onbuttonTapped: () => void } = $props();
 
 	let imageUrl = $state(Promise.resolve(''));
 
@@ -29,36 +27,38 @@
 
 {#await imageUrl then imageUrl}
 	<div
-		class="card w-96 bg-base-200 shadow-xl"
+		class='card w-96 bg-base-200 shadow-xl'
 		in:scale|global={{ delay: 200, duration: 300 }}
 		out:fly|global={{
 			delay: outMoveDirection ? 100 : 0,
 			duration: outMoveDirection ? 400 : 0,
-			x: outMoveDirection
+			x: outMoveDirection,
 		}}
 	>
 		<figure>
-			<img class="h-60 object-cover" src={imageUrl} alt="Dog" in:fade|global />
+			<img class='h-60 object-cover' alt='Dog' src={imageUrl} in:fade|global />
 		</figure>
-		<div class="card-body items-center text-center">
-			<h2 class="card-title">Do You ❤️ This 🐶 ?</h2>
-			<div class="card-actions justify-center">
+		<div class='card-body items-center text-center'>
+			<h2 class='card-title'>Do You ❤️ This 🐶 ?</h2>
+			<div class='card-actions justify-center'>
 				<button
-					class="btn btn-outline btn-primary btn-lg"
+					class='btn btn-outline btn-primary btn-lg'
 					onclick={() => {
 						outMoveDirection = -500;
 						onbuttonTapped();
 					}}
+					type='button'
 				>
 					💔
 				</button>
 				<button
-					class="btn btn-outline btn-secondary btn-lg"
+					class='btn btn-outline btn-secondary btn-lg'
 					onclick={async () => {
 						likedDogsList.add(imageUrl);
 						outMoveDirection = 500;
 						onbuttonTapped();
 					}}
+					type='button'
 				>
 					❤️
 				</button>
