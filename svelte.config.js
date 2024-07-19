b0c1e425aa21b0f71794f0346b10c829a91845d7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-19 20:51:16 +0100

    to adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import path from 'node:path';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @param {...string} args */
